aaeaf19e2c194697cea8649a754314abb1f9903d
DSD3V <>
2022-08-13 17:21:59 -0500

    updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const path = require('path');
 module.exports = {
   devServer: {
     historyApiFallback: true,
+    static: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
   entry: './src/index.tsx',
