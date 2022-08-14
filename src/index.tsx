31a2f8d44a6f704a805aaabddcaa0f891fc6694e
DSD3V <>
2022-08-14 17:22:48 -0500

    updated title/desc.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
 
 import './index.css';
 import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <title>DS.DEV</title>
+      <meta content="DS.DEV" name="description" />
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
