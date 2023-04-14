082c4f88e0f30760edea6ecff2bc73d35595bfac
DS <>
2023-04-13 22:01:52 -0500

    cleared site

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,23 +1,3 @@
-import { HomeNavigation } from './HomeNavigation';
-import {
-  Gradient,
-  H1,
-  HomeDiv,
-  Spotlights,
-  TitleDiv,
-} from '../styles/HomeStyles';
-
 export const Home = () => (
-  <HomeDiv>
-    <TitleDiv>
-      <H1>
-        DS
-      </H1>
-      <Gradient />
-      {Spotlights.map((Spotlight, index) => (
-        <Spotlight key={index} />
-      ))}
-    </TitleDiv>
-    <HomeNavigation />
-  </HomeDiv>
+    <h1>DS</h1>
 );
