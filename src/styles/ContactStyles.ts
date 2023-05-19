07dbb0c3734d9e9827a21bfc0dfd7784f39102ee
DS <ds@DSs-MacBook-Pro.local>
2023-05-19 09:48:04 -0500

    re-added LinkedIn

--- src/styles/ContactStyles.ts
+++ src/styles/ContactStyles.ts
@@ -2,6 +2,7 @@
 import styled from 'styled-components';
 
 import { COLORS } from '../constants';
+import { Link } from '../styles/GlobalStyles';
 
 export const ContactDiv = styled.div`
   align-items: center;
@@ -14,4 +15,9 @@ export const ContactDiv = styled.div`
 
 export const EmailDiv = styled.div`
   color: ${COLORS.LIGHT_GREY};
+`;
+
+export const LinkedInLink = styled(Link)`
+  font-size: 1.4rem;
+  padding-top: 14px;
 `;
